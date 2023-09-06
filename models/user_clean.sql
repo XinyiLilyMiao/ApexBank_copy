a7b03ef927bb0f28344590108916b129a73f4454
Xinyi Miao <miaobaihe2333@gmail.com>
2023-09-06 07:56:17 +0000

    adding top cities

--- models/user_clean.sql
+++ models/user_clean.sql
@@ -6,12 +6,18 @@ SUBSTR(user_id,
 ,birth_year
 ,2019-birth_year AS age
 ,c.country AS country
-,city
-,created_date as member_at
-,Extract(DATE FROM created_date) AS date
+,CASE
+ WHEN REGEXP_CONTAINS(LOWER(city),'london') THEN 'London'
+ WHEN REGEXP_CONTAINS(LOWER(city),'warszawa OR warsaw') THEN 'Warszawa'
+ WHEN REGEXP_CONTAINS(LOWER(city),'paris') THEN 'Paris'
+ WHEN REGEXP_CONTAINS(LOWER(city),'dublin') THEN 'Dublin'
+ WHEN REGEXP_CONTAINS(LOWER(city),'vilnius') THEN 'Vilnius'
+ELSE "other"
+END AS top_city
+,Extract(DATE FROM created_date) AS member_at
 ,Extract(YEAR FROM created_date) AS year
 ,Extract(MONTH FROM created_date) AS month
 ,Extract(DAY FROM created_date) AS day 
 FROM `ApexBank.users` u
 left join `ApexBank.country_code` c
-ON u.country=c.code
+ON u.country=c.code_2
