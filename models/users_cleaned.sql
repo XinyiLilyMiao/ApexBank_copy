a6bd743a57a0796b2ae1d0752481eab3e753b5f8
Leon Band <band.leon@gmail.com>
2023-09-06 09:08:32 +0000

    Expanded Xinyi's users_clean model: 1. Filled null values in "attributes_notifications_marketing_push" and "attributes_notifications_marketing_email" through joining with notifications table. Casted both to boolean

--- models/users_cleaned.sql
+++ models/users_cleaned.sql
@@ -1,38 +1,37 @@
 WITH notifications_filled AS (
     SELECT 
-        DISTINCT user_id,
-        1 AS attributes_notifications_marketing_push_filled,
-        1 AS attributes_notifications_marketing_email_filled
-    FROM notifications
+        DISTINCT user_id
+        ,1 AS attributes_notifications_marketing_push_filled
+        ,1 AS attributes_notifications_marketing_email_filled
+    FROM `iconic-iridium-393108.ApexBank.notifications`
 ),
+
 users_filled AS (
     SELECT
-        COALESCE(users.attributes_notifications_marketing_push, notifications_filled.attributes_notifications_marketing_push_filled) AS attributes_notifications_marketing_push_filled,
-        COALESCE(users.attributes_notifications_marketing_email, notifications_filled.attributes_notifications_marketing_email_filled) AS attributes_notifications_marketing_email_filled
-    FROM users
-    LEFT JOIN notifications_filled
-    ON users.user_id = notifications_filled.user_id
+        u.user_id
+        ,COALESCE(u.attributes_notifications_marketing_push, n.attributes_notifications_marketing_push_filled) AS attributes_notifications_marketing_push_filled
+        ,COALESCE(u.attributes_notifications_marketing_email, n.attributes_notifications_marketing_email_filled) AS attributes_notifications_marketing_email_filled
+    FROM `iconic-iridium-393108.ApexBank.users` AS u
+    LEFT JOIN notifications_filled AS n
+    ON u.user_id = n.user_id
 )
---SELECT * FROM users_filled;
 
 SELECT 
-SUBSTR(user_id, 
-(STRPOS(user_id, '_') + 1), 
-(LENGTH(user_id) - STRPOS(user_id, '_'))
-) AS user_id
-,birth_year
-,2019 - birth_year AS age
-,c.country AS country
-,city
-,created_date as member_at
-,Extract(DATE FROM created_date) AS date
-,Extract(YEAR FROM created_date) AS year
-,Extract(MONTH FROM created_date) AS month
-,Extract(DAY FROM created_date) AS day
-,CAST(user_settings_crypto_unlocked AS BOOLEAN) AS crypto_unlocked
-,plan
-,CAST(users_filled.attributes_notifications_marketing_push_filled AS BOOLEAN) AS notifications_push_enabled
-,CAST(users_filled.attributes_notifications_marketing_email_filled AS BOOLEAN) AS notifications_email_enabled
-,num_contacts
-FROM `ApexBank.users` AS u
-LEFT JOIN `ApexBank.country_code` AS c ON u.country=c.code
+    SUBSTR(u.user_id, (STRPOS(u.user_id, '_') + 1), (LENGTH(u.user_id) - STRPOS(u.user_id, '_'))) AS user_id
+    ,u.birth_year
+    ,2019 - u.birth_year AS age
+    ,cc.country AS country
+    ,u.city
+    ,u.created_date as member_at
+    ,Extract(DATE FROM u.created_date) AS date
+    ,Extract(YEAR FROM u.created_date) AS year
+    ,Extract(MONTH FROM u.created_date) AS month
+    ,Extract(DAY FROM u.created_date) AS day
+    ,CAST(u.user_settings_crypto_unlocked AS BOOLEAN) AS crypto_unlocked
+    ,u.plan
+    ,CAST(uf.attributes_notifications_marketing_push_filled AS INT64) AS notifications_push_enabled
+    ,CAST(uf.attributes_notifications_marketing_email_filled AS INT64) AS notifications_email_enabled
+    ,u.num_contacts
+FROM `iconic-iridium-393108.ApexBank.users` AS u
+LEFT JOIN `iconic-iridium-393108.ApexBank.country_code` AS cc ON u.country = cc.code_2
+LEFT JOIN users_filled AS uf ON u.user_id = uf.user_id
