f646a5963ec51f518a3d1805037b71b2ae5ab248
Xinyi Miao <miaobaihe2333@gmail.com>
2023-09-06 16:33:53 +0000

    change

--- models/stg/notifications_cleaned.sql
+++ models/stg/notifications_cleaned.sql
@@ -1,11 +1,11 @@
 SELECT
-  reason,
-  channel,
-  status,
-  CAST(SUBSTR(u.user_id, (STRPOS(u.user_id, '_') + 1), (LENGTH(u.user_id) - STRPOS(u.user_id, '_'))) AS INT64) AS user_id
-  created_date,
-  Extract(DATE FROM created_date) AS notification_date,
-  CASE
+  ,reason
+  ,channel
+  ,status
+  ,CAST(SUBSTR(u.user_id, (STRPOS(u.user_id, '_') + 1), (LENGTH(u.user_id) - STRPOS(u.user_id, '_'))) AS INT64) AS user_id
+  ,created_date
+  ,Extract(DATE FROM created_date) AS notification_date
+  ,CASE
     WHEN reason IN ('BLACK_FRIDAY','FIFTH_PAYMENT_PROMO') THEN 'Promotions'
     WHEN reason IN ('LOST_CARD_ORDER', 'NO_INITIAL_FREE_PROMOPAGE_CARD_ORDER') THEN 'Card'
     WHEN reason IN ('REENGAGEMENT_ACTIVE_FUNDS', 'PUMPKIN_PAYMENT_NOTIFICATION', 'PREMIUM_ENGAGEMENT_FEES_SAVED', 'ONBOARDING_TIPS_ACTIVATED_USERS', 'PREMIUM_ENGAGEMENT_INACTIVE_CARD', 'NO_INITIAL_CARD_USE', 'NO_INITIAL_CARD_ORDER') THEN 'Information'
