cd10e4a30d257f29eb1c9b1c33a219cb5fed8ca5
Xinyi Miao <miaobaihe2333@gmail.com>
2023-09-06 12:39:16 +0000

    merging and changing columns

--- models/users.sql
+++ models/users.sql
@@ -1,6 +1,12 @@
-SELECT * 
-,DATE_DIFF('2019-05-16', u.member_at, DAY) as days_joined
-FROM {{ref('users_cleaned')}} as u
+WITH u AS (
+    SELECT * 
+    ,DATE_DIFF('2019-05-16', member_at, DAY) as days_joined
+    FROM {{ref('users_cleaned')}}
+),
+t AS (
+    SELECT * FROM {{ref('transactions_cleaned')}}
+    )
+
 
 
 
