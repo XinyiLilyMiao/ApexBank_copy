febd7c59aa324682f7636f43cc4b79b34fdaca2b
Leon Band <band.leon@gmail.com>
2023-09-07 15:53:26 +0000

    1. Added in-line comments for COALESCE methods as they are not 100% straight-forward 2. Set first_transaction_date and last_transaction_date equal to the account creation date when users have not made any transaction at all

--- models/stg/users_cleaned.sql
+++ models/stg/users_cleaned.sql
@@ -9,6 +9,8 @@ WITH notifications_filled AS (
 users_filled AS (
     SELECT
         u.user_id
+        ,Extract(DATE FROM u.created_date) AS member_at
+        -- Returns 1 if user gave consent to be notified or received a notification, else returns 0
         ,COALESCE(u.attributes_notifications_marketing_push, n.attributes_notifications_marketing_push_filled, 0) AS attributes_notifications_marketing_push_filled
         ,COALESCE(u.attributes_notifications_marketing_email, n.attributes_notifications_marketing_email_filled, 0) AS attributes_notifications_marketing_email_filled
     FROM `iconic-iridium-393108.ApexBank.users` AS u
@@ -27,7 +29,7 @@ transaction_dates AS (
 
 SELECT 
     CAST(SUBSTR(u.user_id, (STRPOS(u.user_id, '_') + 1), (LENGTH(u.user_id) - STRPOS(u.user_id, '_'))) AS INT64) AS user_id
-    ,u.birth_year
+    ,u.birth_year AS birth_year
     ,2019 - u.birth_year AS age
     ,cc.country AS country
     ,CASE
@@ -38,18 +40,19 @@ SELECT
         WHEN REGEXP_CONTAINS(LOWER(city),'vilnius') THEN 'Vilnius'
         ELSE "other"
     END AS top_city
-    ,Extract(DATE FROM created_date) AS member_at
-    ,Extract(YEAR FROM created_date) AS year
-    ,Extract(MONTH FROM created_date) AS month
-    ,Extract(DAY FROM created_date) AS day 
-    ,DATE_DIFF('2019-05-16', Extract(DATE FROM created_date), DAY) as days_joined
+    ,Extract(YEAR FROM u.created_date) AS year
+    ,Extract(MONTH FROM u.created_date) AS month
+    ,Extract(DAY FROM u.created_date) AS day 
+    ,DATE_DIFF('2019-05-16', uf.member_at, DAY) as days_joined
     ,CAST(u.user_settings_crypto_unlocked AS INT64) AS crypto_unlocked
-    ,u.plan
+    ,u.plan AS plan
     ,CAST(uf.attributes_notifications_marketing_push_filled AS INT64) AS notifications_push_enabled
     ,CAST(uf.attributes_notifications_marketing_email_filled AS INT64) AS notifications_email_enabled
-    ,u.num_contacts
-    ,l.first_transaction_date
-    ,l.last_transaction_date
+    ,u.num_contacts AS num_contacts
+    -- If the user has not made any transactions, we set the first and last transaction date equal to the date the account was created
+    ,COALESCE(l.first_transaction_date, uf.member_at) AS first_transaction_date
+    ,COALESCE(l.last_transaction_date, uf.member_at) AS last_transaction_date
+    --
     ,DATE_DIFF(l.first_transaction_date, DATE(created_date), DAY) AS days_to_first_transaction
     ,DATE_DIFF(DATE '2019-05-16', l.last_transaction_date, DAY) AS days_since_last_transaction
     ,IF(DATE_DIFF(DATE '2019-05-16', l.last_transaction_date, DAY) >= 90, 1, 0) AS churned
