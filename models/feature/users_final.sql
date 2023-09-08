687e4dd280c57f930e18ce902cdd367f77fa0d8f
Xinyi Miao <miaobaihe2333@gmail.com>
2023-09-08 09:33:59 +0000

    move feature columns into final

--- models/feature/users_final.sql
+++ models/feature/users_final.sql
@@ -29,6 +29,35 @@ activity AS (
     group by user_id
     order by avg_days_inactivity
 ),
+
+ notifications_filled AS (
+    SELECT 
+        DISTINCT user_id
+        ,1 AS attributes_notifications_marketing_push_filled
+        ,1 AS attributes_notifications_marketing_email_filled
+    FROM {{ref("notifications_cleaned")}}
+    WHERE reason_category LIKE ("Pro%")
+),
+
+users_filled AS (
+    SELECT
+        u.user_id
+        -- Returns 1 if user gave consent to be notified or received a notification, else returns 0
+        ,COALESCE(u.old_notifications_push_enabled, n.attributes_notifications_marketing_push_filled, 0) AS attributes_notifications_marketing_push_filled
+        ,COALESCE(u.old_notifications_email_enabled, n.attributes_notifications_marketing_email_filled, 0) AS attributes_notifications_marketing_email_filled
+    FROM {{ref('users_cleaned')}} AS u
+    LEFT JOIN notifications_filled AS n
+    ON u.user_id = n.user_id
+),
+
+transaction_dates AS (
+    SELECT 
+        user_id
+        ,MAX(DATE(date_date)) AS last_transaction_date
+        ,MIN(DATE(date_date)) AS first_transaction_date
+    FROM {{ref('transactions_cleaned')}} 
+    GROUP BY user_id
+),
 avg AS (
     SELECT
         user_id
@@ -41,7 +70,18 @@ SELECT
     ,d.device
     ,COALESCE(activity.avg_days_inactivity, DATE_DIFF('2019-05-16', u.member_at, DAY)) AS avg_days_inactivity
     ,COALESCE(avg.nb_transactions_day, 0) AS nb_transactions_day
+        -- If the user has not made any transactions, we set the first and last transaction date equal to the date the account was created
+    ,COALESCE(l.first_transaction_date, u.member_at) AS first_transaction_date
+    ,COALESCE(l.last_transaction_date, u.member_at) AS last_transaction_date
+    ,uf.attributes_notifications_marketing_push_filled AS notifications_push_enabled
+    ,uf.attributes_notifications_marketing_email_filled AS notifications_email_enabled
+    --
+    ,DATE_DIFF(l.first_transaction_date, u.member_at, DAY) AS days_to_first_transaction
+    ,DATE_DIFF(DATE '2019-05-16', l.last_transaction_date, DAY) AS days_since_last_transaction
+    ,IF(DATE_DIFF(DATE '2019-05-16', l.last_transaction_date, DAY) >= 90, 1, 0) AS churned
 FROM u
 LEFT JOIN activity on u.user_id=activity.user_id
 LEFT JOIN avg on u.user_id=avg.user_id
 LEFT JOIN d on u.user_id=d.user_id
+LEFT JOIN users_filled AS uf ON u.user_id = uf.user_id
+LEFT JOIN transaction_dates AS l ON u.user_id = l.user_id
--- models/stg/users_cleaned.sql
+++ models/stg/users_cleaned.sql
@@ -1,31 +1,3 @@
-WITH notifications_filled AS (
-    SELECT 
-        DISTINCT user_id
-        ,1 AS attributes_notifications_marketing_push_filled
-        ,1 AS attributes_notifications_marketing_email_filled
-    FROM `iconic-iridium-393108.ApexBank.notifications`
-),
-
-users_filled AS (
-    SELECT
-        u.user_id
-        -- Returns 1 if user gave consent to be notified or received a notification, else returns 0
-        ,COALESCE(u.attributes_notifications_marketing_push, n.attributes_notifications_marketing_push_filled, 0) AS attributes_notifications_marketing_push_filled
-        ,COALESCE(u.attributes_notifications_marketing_email, n.attributes_notifications_marketing_email_filled, 0) AS attributes_notifications_marketing_email_filled
-    FROM `iconic-iridium-393108.ApexBank.users` AS u
-    LEFT JOIN notifications_filled AS n
-    ON u.user_id = n.user_id
-),
-
-transaction_dates AS (
-    SELECT 
-        user_id
-        ,MAX(DATE(created_date)) AS last_transaction_date
-        ,MIN(DATE(created_date)) AS first_transaction_date
-    FROM `iconic-iridium-393108.ApexBank.transactions`
-    GROUP BY user_id
-)
-
 SELECT 
     CAST(SUBSTR(u.user_id, (STRPOS(u.user_id, '_') + 1), (LENGTH(u.user_id) - STRPOS(u.user_id, '_'))) AS INT64) AS user_id
     ,u.birth_year AS birth_year
@@ -46,17 +18,8 @@ SELECT
     ,DATE_DIFF('2019-05-16', Extract(DATE FROM u.created_date), DAY) as days_joined
     ,CAST(u.user_settings_crypto_unlocked AS INT64) AS crypto_unlocked
     ,u.plan AS plan
-    ,CAST(uf.attributes_notifications_marketing_push_filled AS INT64) AS notifications_push_enabled
-    ,CAST(uf.attributes_notifications_marketing_email_filled AS INT64) AS notifications_email_enabled
+    ,CAST(u.attributes_notifications_marketing_push AS INT64) AS old_notifications_push_enabled
+    ,CAST(u.attributes_notifications_marketing_email AS INT64) AS old_notifications_email_enabled
     ,u.num_contacts AS num_contacts
-    -- If the user has not made any transactions, we set the first and last transaction date equal to the date the account was created
-    ,COALESCE(l.first_transaction_date, Extract(DATE FROM u.created_date)) AS first_transaction_date
-    ,COALESCE(l.last_transaction_date, Extract(DATE FROM u.created_date)) AS last_transaction_date
-    --
-    ,DATE_DIFF(l.first_transaction_date, DATE(created_date), DAY) AS days_to_first_transaction
-    ,DATE_DIFF(DATE '2019-05-16', l.last_transaction_date, DAY) AS days_since_last_transaction
-    ,IF(DATE_DIFF(DATE '2019-05-16', l.last_transaction_date, DAY) >= 90, 1, 0) AS churned
 FROM `iconic-iridium-393108.ApexBank.users` AS u
 LEFT JOIN `iconic-iridium-393108.ApexBank.country_code` AS cc ON u.country = cc.code_2
-LEFT JOIN users_filled AS uf ON u.user_id = uf.user_id
-LEFT JOIN transaction_dates AS l ON u.user_id = l.user_id
